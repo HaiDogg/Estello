c7f0f2d2558f873ea98b4a352816f1d85a9b017d
HDGG <haidole.hdl@gmail.com>
2019-04-04 13:51:51 +0200

    benutzer klasse

---
+++ app/src/main/java/com/example/hdgg/estello/Benutzer.java
@@ -0,0 +1,39 @@
+package com.example.hdgg.estello;
+
+public class Benutzer {
+    private static int benutzer_id = 0;
+    private String benutzer_name;
+    private String benutzer_email;
+    private String benutzer_pw;
+
+
+
+public Benutzer(){
+
+    }
+
+    public BenutzerErstellen(String benutzer_name, String benutzer_email, String benutzer_pw){
+
+        this.benutzer_name = benutzer_name;
+        this.benutzer_email = benutzer_email;
+        this.benutzer_pw = benutzer_pw;
+
+    }
+
+    public static int getBenutzer_id() {
+        return benutzer_id;
+    }
+
+    public void setBenutzer(String benutzer_name){
+    this.benutzer_name = benutzer_name;
+
+    }
+
+    public String getBenutzer_name(){
+        return benutzer_name;
+    }
+
+    public String getBenutzer_email(){
+        return benutzer_email;
+    }
+}
