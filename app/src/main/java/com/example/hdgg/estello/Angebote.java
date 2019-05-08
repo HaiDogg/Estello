0a7f8429dab72fa7b713e48a4a816e8184e96858
esti96 <estelleweinstock@yahoo.de>
2019-05-08 15:50:42 +0200

    name des kaufes wird nicht angenommen

--- app/src/main/java/com/example/hdgg/estello/Angebote.java
+++ app/src/main/java/com/example/hdgg/estello/Angebote.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -58,7 +59,7 @@ public class Angebote extends AppCompatActivity {
 
 
 
-    
+
     public int holGuthaben(String newString){
 
 
@@ -86,7 +87,7 @@ public class Angebote extends AppCompatActivity {
     *
     * */
 
-    public void kaufMich(){
+    public void kaufMich(final View view){
 
         //guthaben von kunde geholt mit dem namen den wir übergeben kriegen
 
@@ -103,6 +104,7 @@ public class Angebote extends AppCompatActivity {
                     // the user clicked on colors[which]
                     if(which == 0){
                         machWasZuTunIst(guthaben,preis);
+                        Log.d("TAG","hahah");
                     }
                     else{
                         Intent i = new Intent(Angebote.this,Angebote.class);
@@ -141,6 +143,12 @@ public class Angebote extends AppCompatActivity {
         //helper.updateGuthabenDB(guthaben, benutzer_name);
         TextView angebot_name = (TextView) findViewById(R.id.angebote);//nochmal strings für die Angebote einfuegen
         String a_name = angebot_name.getText().toString();
+
        // profil.updateListe(a_name);
+        // intent mit dem der name vom kauf übergeben wird.
+        String kaufName = "Netflix";
+        Intent i = new Intent(Angebote.this, Profil.class);
+        i.putExtra("kauf", kaufName);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/hdgg/estello/Profil.java
+++ app/src/main/java/com/example/hdgg/estello/Profil.java
@@ -3,9 +3,11 @@ package com.example.hdgg.estello;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
 
 
 public class Profil extends AppCompatActivity {
@@ -13,6 +15,8 @@ public class Profil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
         welcherName();
+        Log.d("oncreate","oncreate methode von profil");
+        welcherKauf();
     }
 
     public void gehZuAngebote(final View view){
@@ -30,6 +34,23 @@ public class Profil extends AppCompatActivity {
         return newString;
     }
 
+    public void welcherKauf(){
+        Log.d("wk","welcher kauf");
+        try {
+            String kauf;
+            Bundle extra = getIntent().getExtras();
+            kauf =extra.getString("kauf");
+            zeigKaufean(kauf);
+        }catch(Exception e) {
+            Log.d("Kein Kauf","Noch kein Kauf");
+        }
+    }
+
+    public void zeigKaufean(String kauf){
+        TextView kaufe = (TextView) findViewById(R.id.kaufe_id);
+        kaufe.setText(kauf);
+    }
+
     public void zeigNamean(String newString){
 
         TextView tv_name = (TextView) findViewById(R.id.profil_name_id);
@@ -44,4 +65,3 @@ public class Profil extends AppCompatActivity {
         textView.setText(old + name_von_textview);
     }
 }
-
